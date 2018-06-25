b4b4be29b1acc373a9e8cb6d6c03252b2258eaa5
DamienPoix <damienpoix@live.fr>
2018-06-25 10:37:22 +0200

    Exercice 6 Part2

---
+++ assets/js/fonction.js
@@ -0,0 +1,31 @@
+function getImage1(){
+ document.getElementById ('image1') .src='assets/img/image1_2.jpg';
+}
+function getImage2(){
+ document.getElementById ('image2') .src='assets/img/image2_2.jpg';
+}
+function getImage3(){
+ document.getElementById ('image3') .src='assets/img/image3_2.jpg';
+}
+function getImage4(){
+ document.getElementById ('image4') .src='assets/img/image4_2.jpg';
+}
+function getImage5(){
+ document.getElementById ('image5') .src='assets/img/image5_2.jpg';
+}
+//function
+function outImageA(){
+ document.getElementById('image1') .src='assets/img/image1.jpg';
+}
+function outImage2(){
+ document.getElementById ('image2') .src='assets/img/image2.jpg';
+}
+function outImage3(){
+ document.getElementById ('image3') .src='assets/img/image3.jpg';
+}
+function outImage4(){
+ document.getElementById ('image4') .src='assets/img/image4.jpg';
+}
+function outImage5(){
+ document.getElementById ('image5') .src='assets/img/image5.jpg';
+}
